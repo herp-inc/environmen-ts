7d59a0a0043fc41de54c1bdc0d1c8733eb479cfd
Ryota Kameoka <ok@ryota-ka.me>
2023-01-24 15:30:28 +0900

    Test that `number()` rejects infinities

--- src/VariableDecoder/number.test.ts
+++ src/VariableDecoder/number.test.ts
@@ -1,6 +1,7 @@
 import * as fc from 'fast-check';
 import * as E from 'fp-ts/Either';
 
+import { DecodeFailed } from '../EnvironmentError';
 import { Variable } from '../Variable';
 
 import { number } from './number';
@@ -10,10 +11,19 @@ describe(number, () => {
         const decoder = number();
 
         fc.assert(
-            fc.property(fc.float(), (n) => {
-                const variable = new Variable('KEY', String(n));
-                expect(decoder(variable)).toStrictEqual(E.right(n));
-            }),
+            fc.property(
+                fc.float().filter((x) => Number.isFinite(x)),
+                (n) => {
+                    const variable = new Variable('KEY', String(n));
+                    expect(decoder(variable)).toStrictEqual(E.right(n));
+                },
+            ),
         );
+    });
+
+    test.each([Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY])('rejects "%s"', (str) => {
+        const decoder = number();
+        const variable = new Variable('KEY', String(str));
+        expect(decoder(variable)).toStrictEqual(E.left(new DecodeFailed(variable, 'must be finite')));
     });
 });
